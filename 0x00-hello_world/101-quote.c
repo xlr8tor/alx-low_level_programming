92a379597590d1ab25a9032d3f1892234721796f
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-02-16 02:43:26 -0800

    correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	char[] error = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char error[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	fwrite(error, 58, 1, stderr);
 	return (1);
 }
