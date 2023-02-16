8f6ae0e1dedd24ae4d08f4de9dcd0a1d11fb4aed
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-02-16 02:41:47 -0800

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: 1
+*/
+
+int main(void)
+{
+	char[] error = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	fwrite(error, 58, 1, stderr);
+	return (1);
+}
