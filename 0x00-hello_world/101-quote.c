b175e188f4ecbaede26d8366f87529d3e1cc0434
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-02-16 02:50:42 -0800

    Add newline

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	char error[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char error[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	fwrite(error, sizeof(error) - 1, 1, stderr);
 	return (1);
 }
