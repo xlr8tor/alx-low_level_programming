3eb3d68d9c9a38dad7f80cfd0fabaa7e936c030e
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-02-16 02:53:39 -0800

    Fix betty corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,8 @@
 
 int main(void)
 {
-	char error[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	fwrite(error, sizeof(error) - 1, 1, stderr);
+	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fwrite(err, sizeof(err) - 1, 1, stderr);
 	return (1);
 }
