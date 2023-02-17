6f4a29e8393ed4fcc6439128ab8228ecf75b5cac
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-02-17 10:47:46 -0800

    Digits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Print combination of numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 99; ++i)
+	{
+		for (j = i; j <= 99; ++j)
+		{
+			int n1 = i / 10;
+			int n2 = i % 10;
+			int n3 = j / 10;
+			int n4 = j % 10;
+
+			putchar(n1 + '0');
+			putchar(n2 + '0');
+			putchar(' ');
+			putchar(n3 + '0');
+			putchar(n4 + '0');
+
+			if (i < 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
