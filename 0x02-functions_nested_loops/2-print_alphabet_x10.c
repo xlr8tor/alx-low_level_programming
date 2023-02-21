0d47c339a0d00bd2e2e66fdd594b72f7de932988
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-02-20 23:17:07 -0800

    print alphabets x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Prints lowercase alphabets 10 times
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void);
+
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
+
+/**
+ * print_alphabet_x10 -  Prints lowercase alphabets 10 times
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10; ++i)
+	{
+		for (c = 'a'; c <= 'z'; ++c)
+                	putchar(c);
+        	putchar('\n');
+	}
+}
