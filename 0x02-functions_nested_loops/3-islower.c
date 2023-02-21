f993a4934dd8bd1cffc82605442af8eec01c76b7
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-02-21 03:57:19 -0800

    Fix betty

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,29 +1,4 @@
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: 0
- */
-
-/**
- * @c: integer number
- */
-int _islower(int c);
-
-int main(void)
-{
-	int r;
-
-	r = _islower('H');
-	putchar(r + '0');
-	r = _islower('o');
-	putchar(r + '0');
-	r = _islower(108);
-	putchar(r + '0');
-	putchar('\n');
-	return (0);
-}
+#include "main.h"
 
 /**
  * _islower - Check if arg is lowercase
