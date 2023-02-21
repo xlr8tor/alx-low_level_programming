071a9838b7deb995a61be165d5ac4c7ac3622eab
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-02-21 09:41:45 -0800

    test

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - rints the first 50 Fibonacci numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	unsigned long first;
+	unsigned long second;
+	unsigned long next;
+
+	first = 1;
+	second = 2;
+
+	printf("%lu, ", first);
+	printf("%lu, ", second);
+
+	for (i = 0; i < 98; ++i)
+	{
+		next = second;
+		second = second + first;
+		first = next;
+
+		printf("%lu", second);
+
+		if (i < 97)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
