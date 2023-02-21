070736ffcfa5d4321b994a89f83a3da1bfa871f8
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-02-21 04:08:40 -0800

    Fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,42 +1,4 @@
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: 0
- */
-
-/**
- * @c: integer number
- */
-int print_sign(int c);
-
-int main(void)
-{
-	int r;
-
-	r = print_sign(98);
-	putchar(',');
-	putchar(' ');
-	putchar(r + '0');
-	putchar('\n');
-	r = print_sign(0);
-	putchar(',');
-	putchar(' ');
-	putchar(r + '0');
-	putchar('\n');
-	r = print_sign(0xff);
-	putchar(',');
-	putchar(' ');
-	putchar(r + '0');
-	putchar('\n');
-	r = print_sign(-1);
-	putchar(',');
-	putchar(' ');
-	putchar(r + '0');
-	putchar('\n');
-	return (0);
-}
+#include "main.h"
 
 /**
  * print_sign - print sign of a number
@@ -48,17 +10,17 @@ int print_sign(int c)
 {
 	if (c > 0)
 	{
-		putchar('+');
+		_putchar('+');
 		return (1);
 	}
 	else if (!c)
 	{
-		putchar('0');
+		_putchar('0');
 		return (0);
 	}
 	else
 	{
-		putchar('-');
+		_putchar('-');
 		return (-1);
 	}
 }
