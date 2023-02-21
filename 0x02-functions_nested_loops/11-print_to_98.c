ffba02dd6d06a7eab506877f31733075748d5866
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-02-21 06:38:41 -0800

    Fix betty

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,11 +8,12 @@
 void print_to_98(int n)
 {
 	int i;
+
 	if (n <= 98)
 	{
 		for (i = n; i < 99; i++)
-		{	
-			printf("%d",i);
+		{
+			printf("%d", i);
 			if (i < 98)
 			{
 				putchar(',');
@@ -25,12 +26,12 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; --i)
 		{
-		 	printf("%d",i);
-                        if (i > 98)
-                        {
-                                putchar(',');
-                                putchar(' ');
-                        }
+			printf("%d", i);
+			if (i > 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 
