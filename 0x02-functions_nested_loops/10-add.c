b81f39404c9ba34942b44fbbb2eeb5ac9856ba6d
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-02-21 01:41:49 -0800

    add func

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+
+int add(int, int);
+
+/**
+ * add - Adds two numbers and returns result
+ *
+ * @num1: First argument
+ * @num2: Second argument
+ *
+ * Return: num1 + num2
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
