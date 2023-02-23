6b732292c92c26874fdacd3bf179019d57c8ba8d
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-02-23 08:19:33 -0800

    test

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,14 +6,38 @@
 
 void print_number(int n)
 {
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
+	    int last_d = -1;
+    int pow = 1;
+    int d;
 
-	if (n / 10)
-		print_number(n / 10);
+    if (n < 0)
+    {
+        _putchar('-');
+        last_d = '0' - (n % 10);
+        n /= 10;
+        n = -n;
+    }
+    else if (n > 9)
+    {
+        last_d = '0' + (n % 10);
+        n /= 10;
+    }
+    else
+    {
+        _putchar(n + '0');
+    }
 
-	_putchar(n % 10 + '0');
+    while (pow * 10 <= n)
+        pow *= 10;
+
+    while (pow != 0)
+    {
+        d = n / pow;
+        if (n)
+            _putchar(d + '0');
+        n = n - (d * pow);
+        pow /= 10;
+    }
+    if (last_d != -1)
+        _putchar(last_d);
 }
