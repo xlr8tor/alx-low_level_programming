7afb3081705d128179965d9b4e8298e69a251650
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-02-27 01:33:58 -0800

    puts 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other char in string
+ * @str: string to print
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
