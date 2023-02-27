2bf4862a6b48f980af5727b5127d1ebb1d35330a
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-02-27 10:42:32 -0800

    fix

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,36 +8,23 @@
 
 int _atoi(char *s)
 {
-	int pow  = 1;
-	int i, j, tmp, sign, num;
+	unsigned int num;
+	int sign;
+	int i;
 
-	sign = num = 0;
+	num = 0;
+	sign = 1;
 
 	for (i = 0; s[i] != '\0'; i++)
-		;
-
-	for (j = 0; j < i / 2; j++)
 	{
-		tmp = s[j];
-		s[j] = s[i - (j + 1)];
-		s[i - (j + 1)] = tmp;
-	}
-
-	for (j = 0; j < i; j++)
-	{
-		if (s[j] == '-')
-			sign++;
-		else if (s[j] >= '0' && s[j] <= '9')
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			num += (s[j] - '0') * pow;
-			pow *= 10;
+			num = num * 10 + (s[i] - '0');
+			if (s[i + 1] < 48 || s[i + 1] > 57)
+				break;
 		}
-		else
-		{
-			continue;
 		}
-	}
-	num = sign % 2 == 0 ? num : -num;
-	return (num);
-
+	return (num * sign);
 }
