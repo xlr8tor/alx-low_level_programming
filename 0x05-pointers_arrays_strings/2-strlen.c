7d2d031bc3b3a8b9b9016f7c95d85b64e42bf975
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-02-27 00:30:15 -0800

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+/**
+ * _strlen - returns the length of a string.
+ * @s: Argument
+ *
+ * Return: length of s
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
+}
