2e750ed6e829f474d33f419e645203cf4d47c3b9
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-02-27 09:32:33 -0800

    advanced

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * _atoi - coverts string to integer
+ * @s: string to covert
+ *
+ * Return: (int) s
+ */
+
+int _atoi(char *s)
+{
+	int pow  = 1;
+	int i, j, tmp, sign, num;
+
+	sign = num = 0;
+
+	for (i = 0; s[i] != '\0'; i++);
+
+	for (j = 0; j < i / 2; j++)
+	{
+		tmp = s[j];
+		s[j] = s[i - (j + 1)];
+		s[i - (j + 1)] = tmp;
+	}
+
+	for (j = 0; j < i; j++)
+	{
+		if (s[j] == '-')
+			sign++;
+		else if (s[j] >= '0' && s[j] <= '9')
+		{
+			num += (s[j] - '0') * pow;
+			pow *= 10;
+		}
+		else
+		{
+			continue;
+		}
+	}
+	num = sign % 2 == 0 ? num : -num;
+	return (num);
+
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -23,4 +23,6 @@ void print_array(int *a, int n);
 
 char *_strcpy(char *dest, char *src);
 
+int _atoi(char *s);
+
 #endif /* _SAMPLE_HEADER_H_ */
