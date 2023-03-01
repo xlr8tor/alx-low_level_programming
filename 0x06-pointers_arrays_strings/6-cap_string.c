f651a47c5a6f2d45e072b599674a95c81fe071a0
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-03-01 06:50:06 -0800

    6-cap_string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,51 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string to capitalize
+ *
+ * Return: s
+ */
+
+int _islower(char c);
+
+int _islower(char c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	return (0);
+}
+
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0 && _islower(s[i]))
+		{
+			s[i] = s[i] - 'a' + 'A';
+			continue;
+		}
+		switch (s[i])
+		{
+			case ' ':
+			case '\n':
+			case '\t':
+			case '.':
+			case ',':
+			case '!':
+			case '?':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+				if (_islower(s[i + 1]))
+					s[i + 1] = s[i + 1] - 'a' + 'A';
+				break;
+			default:
+				break;
+
+		}
+	}
+	return (s);
+}
