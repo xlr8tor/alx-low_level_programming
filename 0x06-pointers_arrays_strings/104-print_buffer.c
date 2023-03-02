beb4093ff4131d6acaff8264b4195c5fd3974f4b
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-03-02 01:03:07 -0800

    104 buffer

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_buffer - prints the contents of a buffer
+ * @b: input string
+ * @size: size to print
+ */
+
+void print_buffer(char *b, int size)
+{
+	int i, j, c;
+
+	c = 0;
+	for (i = 0; i < size; i += 10)
+	{
+		printf("00000000%02x: ", i);
+		for (j = 0; j < 10; j++)
+		{
+			if (c >= size)
+				printf("  ");
+			else
+				printf("%02x", *(b + c));
+			if (j % 2 != 0)
+				printf(" ");
+			c++;
+		}
+		c -= 10;
+
+		for (j = 0; j < 10 && c < size; j++)
+		{
+			printf("%c", *(b + c) > 31 && *(b + c) < 127 ? *(b + c) : '.');
+			c++;	
+		}
+		printf("\n");
+
+	}	
+}
