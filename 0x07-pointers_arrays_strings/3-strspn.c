c679df35cfcc4c5f3e48bc2c6084f011e000f639
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-03-06 03:24:14 -0800

    Fix betty

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,9 +1,16 @@
 #include "main.h"
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string
+ * @accept: substring
+ *
+ * Return: length of prefix
+ */
 
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j, count = 0;
-	
+
 	for (i = 0; s[i] != '\0' && s[i] != ' '; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
