015c6eb9e324ec483b25de4cc018a90a2709a580
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-03-08 00:42:39 -0800

    Fix betty

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,15 +2,18 @@
 /**
  * _strlen_recursion - finds length of string
  * @s: string to find length
+ *
+ * Return: length of the string
  */
 
 int _strlen_recursion(char *s)
 {
 	int len = 0;
+
 	if (!(*s))
 		return (0);
-	
+
 	len = _strlen_recursion(s + 1) + 1;
-	
+
 	return (len);
 }
---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ * factorial - returns factorial of a number
+ * @n: find factorial of n
+ *
+ * Return: !n
+ */
