63959ed1845c1e7f578e69fc8bc26a6d4f1d0f05
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-03-07 22:45:04 -0800

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string followed by \n
+ * @s: string to print
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	s++;
+
+	if (*s)
+		_puts_recursion(s);
+	_putchar('\n');
+}
