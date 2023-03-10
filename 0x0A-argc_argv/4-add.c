54325a9a93e3f98e7d1ca043b497799fd98e6997
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-03-10 02:13:20 -0800

    4-add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * main - Prints name of program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (isdigit(*argv[i]))
+		{
+			sum += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
