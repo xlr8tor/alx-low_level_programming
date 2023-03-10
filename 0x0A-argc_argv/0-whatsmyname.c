90815d812b17074d39ff721ebb45e0afb0acd71e
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-03-10 01:42:15 -0800

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Prints name of program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	(void) argc;
+	printf("%s\n", *argv);
+
+	return (0);
+}
