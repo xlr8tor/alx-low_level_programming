682cf087188a3cc785215f6df314f7001d4fd789
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-03-13 06:11:12 -0700

    Hotfix

--- 0x0B-malloc_free/2-main.c
+++ 0x0B-malloc_free/2-main.c
@@ -11,7 +11,7 @@ int main(void)
 {
     char *s;
 
-    s = str_concat("", "Holberton");
+    s = str_concat(NULL, "Betty");
     if (s == NULL)
     {
         printf("failed\n");
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,14 +14,15 @@ char *str_concat(char *s1, char *s2)
 	int len1 = 0, len2 = 0;
 	int i, j, size;
 
-	while (s1[len1] != '\0')
-		++len1;
-	while (s2[len2] != '\0')
-		++len2;
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+
+	while (s1[len1] != '\0')
+		++len1;
+	while (s2[len2] != '\0')
+		++len2;
 
 	size = len1 + len2 + 1;
 
