72cd2c2d9bb3c0f51e145b89a3e4ecde61e60a4c
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-03-13 02:35:51 -0700

    handle malloc return

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,9 @@ char *create_array(unsigned int size, char c)
 	if (size < 1)
 		return (NULL);
 	arr = malloc(sizeof(char) * size);
-
+	
+	if (arr == NULL)
+		return (1);
 	for (i = 0; i < size; i++)
 		arr[i] = c;
 
