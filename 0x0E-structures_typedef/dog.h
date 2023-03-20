db927cb5df0fd947a27b68b3a48f83a7013e8155
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-03-20 01:26:32 -0700

    dog header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * struct dog - blueprint for a dog instance
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner's name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _MAIN_H_ */
