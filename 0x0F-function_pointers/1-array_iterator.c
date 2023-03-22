68054d77e8709e5283884b247843776b37930c66
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-03-22 04:17:56 -0700

    Fix NULL array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
 
-	if ((int) size <= 0 || (action == NULL))
+	if ((int) size <= 0 || (action == NULL) || (array == NULL))
 		return;
 	for (i = 0; i < (int) size; i++)
 		action(*(array + i));
