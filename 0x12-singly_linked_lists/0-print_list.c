42aa96258a0aebc620d1cf95d5b1af6a19ecd7d7
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-03-30 02:25:43 -0700

    print linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * print_list - prints nodes in a linked list
+ * @h: first node
+ * Return: length of list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *ptr = NULL;
+
+	ptr = h;
+
+	while (ptr != NULL)
+	{
+		count++;
+		if (ptr->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("%s\n", ptr->str);
+		ptr = ptr->next;
+	}
+
+	return (count);
+}
