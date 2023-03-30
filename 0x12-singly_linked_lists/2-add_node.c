b92739525b2ae0d8bd1cc16f6a928f367b3ae9c7
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-03-30 07:10:58 -0700

    test 4

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,19 +9,15 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *ptr;
 	size_t len = 0;
-	char *n;
 
 	ptr = malloc(sizeof(list_t));
 	if (ptr == NULL)
 		return (NULL);
-	n = strdup(str);
-	if (n == NULL)
-		return (NULL);
-	ptr->str = n;
 
-	while (str[++len] != '\0')
-		;
+	while (str[len])
+		len++;
 
+	ptr->str = strdup(str);
 	ptr->len = len;
 	ptr->next = *head;
 	*head = ptr;
