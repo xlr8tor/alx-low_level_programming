3aab5b63fdefa0f9a15d5abbfbebdd63f5d53c81
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-04-03 10:30:36 -0700

    Free test

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,6 +7,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *curr, *next;
 
+	if (*head == NULL)
+	{
+		free(*head);
+		*head = NULL;
+		return;
+	}
+
 	curr = *head;
 	while (curr != NULL)
 	{
