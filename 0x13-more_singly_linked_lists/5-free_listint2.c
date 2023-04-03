36d3e5656b4003ac2f4b1de9fc969b03b5dd9793
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-04-03 02:11:25 -0700

    Free list memory 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: head of list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *curr, *next;
+
+	curr = *head;
+	while (curr != NULL)
+	{
+		next = curr->next;
+		free(curr);
+		curr = next;
+	}
+}
