b66b1fa69c6fc0286348541642529f496397760a
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-04-04 03:39:56 -0700

    Free linked list with loop

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * free_listint_safe - frees a listint_t list
+ * @h: pointer to head node
+ * Return: number of nodes freed
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t count = 0;
+	listint_t *current, *temp;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	current = *h;
+	while (current != NULL)
+	{
+		temp = current->next;
+		count++;
+
+		if (current <= temp)
+		{
+			current->next = NULL;
+			break;
+		}
+		free(current);
+		current = temp;
+
+	}
+
+	*h = NULL;
+	return (count);
+}
