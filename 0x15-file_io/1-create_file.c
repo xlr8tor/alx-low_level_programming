6294035c9482702c9ab7213960f0b0ea5247f40a
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-04-10 02:35:55 -0700

    test

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fdesc = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fdesc = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fdesc < 0)
 		return (-1);
 
