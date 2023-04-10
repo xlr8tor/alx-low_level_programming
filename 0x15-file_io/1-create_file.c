5493b9e835af194bafe80f574098c38485d58133
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-04-10 02:45:51 -0700

    Test 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fdesc;
-	ssize_t len = 0, write_handle;
+	int len = 0, write_handle;
 
 	if (filename == NULL)
 		return (-1);
@@ -18,19 +18,15 @@ int create_file(const char *filename, char *text_content)
 	if (fdesc < 0)
 		return (-1);
 
-	while (text_content[len])
-		len++;
-
 	if (!text_content)
 	{
-		write_handle = write(fdesc, text_content, len);
-		if (write_handle < len)
-		{
-			close(fdesc);
-			return (-1);
-		}
+		while (text_content[len])
+			len++;
 	}
-
+	
+	write_handle = write(fdesc, text_content, len);
+	if (write_handle < 0)
+		return (-1);
 	close(fdesc);
 	return (1);
 }
