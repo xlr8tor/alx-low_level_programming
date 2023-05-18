62e8805b8a44370041178d3bd681ac49d33fb83e
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-05-18 06:46:30 -0700

    Task 7

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - insert node at specied index
+ * @h: pointer to head of list
+ * @idx: position to insert
+ * @n: Data
+ *
+ * Return: inserted node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int count = 0;
+	dlistint_t *ptr, *next, *prev = NULL;
+
+	next = malloc(sizeof(dlistint_t));
+	if (next == NULL || !h)
+	{
+		free(next);
+		return (NULL);
+	}
+	next->n = n;
+	next->next = NULL;
+	next->prev = NULL;
+
+	ptr = *h;
+	while (ptr != NULL && count < idx)
+	{
+		prev = ptr;
+		ptr = ptr->next;
+		count++;
+	}
+	if (count < idx)
+	{
+		free(next);
+		return (NULL);
+	}
+	if (prev == NULL)
+	{
+		next->next = *h;
+		*h = next;
+	}
+	else
+	{
+		prev->next = next;
+		next->next = ptr;
+		next->prev = prev;
+		ptr->prev = next;
+	}
+	return (next);
+}
