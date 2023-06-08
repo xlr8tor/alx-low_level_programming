3bfa28feb615501f7ba76d51d4adbb383216c778
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2023-06-08 11:54:03 -0700

    Test

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 	hash_table_t *table = malloc(sizeof(hash_table_t));
 
-	if (size <= 0)
+	if (size == 0)
 		return (NULL);
 	if (!table)
 		return (NULL);
@@ -20,7 +20,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!(table->array))
 		return (NULL);
 
-	for (i = 0; i < table->size; i++)
+	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
 
 	return (table);
